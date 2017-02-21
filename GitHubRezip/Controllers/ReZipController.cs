33576a142ebfc5653256b8e6bc80ca3a2a4af528
Faiz Shaikh <fashaikh@microsoft.com>
2017-02-21 13:55:02 -0800

    add more flexibility to deploy .zip urls

--- GitHubRezip/Controllers/ReZipController.cs
+++ GitHubRezip/Controllers/ReZipController.cs
@@ -9,47 +9,61 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.IO.Compression;
 using Ionic.Zip;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
 
 namespace GitHubRezip.Controllers
 {
     public class ReZipController : ApiController
     {
+        private static TelemetryClient telemetry = new TelemetryClient();
 
         [HttpGet]
         // GET: /api/ReZip/Azure-Samples/functions-dotnet-sas-token/archive/master
-        public async Task<HttpResponseMessage> GetZip( string user ,
-            string project,
-            string branch
-            )
+        public async Task<HttpResponseMessage> GetZip(string user, string project, string branch)
         {
-            var githubZipUrl = $"https://github.com/{user}/{project}/archive/{branch}.zip";
-            HttpResponseMessage response = Request.CreateResponse();
-            var rzm = new RemoteZipManager(githubZipUrl);
-            var zipstream = await rzm.GetZipStream(githubZipUrl);
-            var zipName = $"{project}-{branch}.zip";
-            response.Content = ZipStreamContent.Create(ref zipstream, ref zipName, zip =>
+            telemetry.TrackEvent("Re-Zip");
+            // Establish an operation context and associated telemetry item:
+            using (var operation = telemetry.StartOperation<RequestTelemetry>("Re-Zip"))
             {
+                branch = branch.ToLowerInvariant().Replace(".zip", String.Empty);
+                telemetry.TrackEvent("Re-Zip Started", new Dictionary<string, string> { { "user", user }, { "project", project} , { "branch", branch} }, null);
 
-                var ziparchive = new ZipArchive(zipstream);
-                var rootFolderFound = false;
-                var rootFolderName = String.Empty;
-                foreach (ZipArchiveEntry e in ziparchive.Entries)
+                var githubZipUrl = $"https://github.com/{user}/{project}/archive/{branch}.zip";
+                HttpResponseMessage response = Request.CreateResponse();
+                var rzm = new RemoteZipManager(githubZipUrl);
+                var zipstream = await rzm.GetZipStream(githubZipUrl);
+                var zipName = $"{project}-{branch}.zip";
+                response.Content = ZipStreamContent.Create(ref zipstream, ref zipName, zip =>
                 {
-                    if (!rootFolderFound && e.Length == 0)
-                    {
-                        rootFolderName = e.FullName;
-                        rootFolderFound = true;
-                    }
-                    else
-                    {
-                        zip.AddFileContent(e.FullName.Replace(rootFolderName, String.Empty), e.Open());
-                    }
-                }
 
-            });
-            return response;
+                    var ziparchive = new ZipArchive(zipstream);
+                    var rootFolderFound = false;
+                    var rootFolderName = String.Empty;
+                    foreach (ZipArchiveEntry e in ziparchive.Entries)
+                    {
+                        if (!rootFolderFound && e.Length == 0)
+                        {
+                            rootFolderName = e.FullName;
+                            rootFolderFound = true;
+                        }
+                        else
+                        {
+                            zip.AddFileContent(e.FullName.Replace(rootFolderName, String.Empty), e.Open());
+                        }
+                    }
+
+                });
+
+                // Set properties of containing telemetry item - for example:
+                operation.Telemetry.ResponseCode = "200";
+
+                //optional
+                telemetry.StopOperation(operation);
+
+                return response;
+            }
         }
-        
 
     }
 }
--- GitHubRezip/Global.asax.cs
+++ GitHubRezip/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Microsoft.ApplicationInsights.Extensibility;
 using Swashbuckle.Application;
 
 namespace GitHubRezip
@@ -13,12 +14,25 @@ namespace GitHubRezip
     {
         protected void Application_Start()
         {
+            InitApplicationInsight();
             //GlobalConfiguration.Configure(WebApiConfig.Register);
             var config = GlobalConfiguration.Configuration;
             GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
             RegisterRoutes(config);
         }
-
+        private void InitApplicationInsight()
+        {
+            var key = Environment.GetEnvironmentVariable("githubrezipAIKey");
+            if (!string.IsNullOrEmpty(key))
+            {
+                TelemetryConfiguration.Active.InstrumentationKey = key;
+                TelemetryConfiguration.Active.DisableTelemetry = false;
+            }
+            else
+            {
+                TelemetryConfiguration.Active.DisableTelemetry = true;
+            }
+        }
         private void RegisterRoutes(HttpConfiguration config)
         {
             config.Routes.MapHttpRoute("rezip", "{user}/{project}/{branch}", new { controller = "ReZip", action = "GetZip", authenticated = false }, new { verb = new HttpMethodConstraint(HttpMethod.Get.ToString()) });
